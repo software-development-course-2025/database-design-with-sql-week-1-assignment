813a8a84de0d7c9f6bdb9a58f82e862e74154a48
Augusto047 <mate.augusto.mz@gmail.com>
2025-09-16 16:42:35 +0200

    Add SQL solutions for database creation and deletion
    Added the SQL queries required to solve the introductory database assignment:
    
    - CREATE DATABASE salesDB;
    - DROP DATABASE demo;
    
    These queries demonstrate the ability to create and remove databases using standard SQL syntax. Comments included for clarity.

---
+++ answers.sql
@@ -0,0 +1,9 @@
+-- 1. Creating a database called 'salesDB'
+-- This command creates a database where we can store sales information.
+
+CREATE DATABASE salesDB;
+
+-- 2. Deleting a database called 'demo'
+-- The command below permanently deletes the database called 'demo'.
+
+DROP DATABASE demo;
